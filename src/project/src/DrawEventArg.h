7b09f80b525dc34a52f54479f497ce2582db3d33
jefa55@yahoo.com <jefa55@yahoo.com@49a7ce7b-0bdc-42b8-8dfc-eafa0b0813a6>
2010-10-26 14:02:11 +0000

    Falto el nuevo evento de Draw (reemplaza al pointeventarg que fue removido)

---
+++ src/project/src/DrawEventArg.h
@@ -0,0 +1,15 @@
+#ifndef POINTEVENT_H_INCLUDED
+#define POINTEVENT_H_INCLUDED
+
+#include "EventArg.h"
+
+class DrawEventArg : public EventArg {
+  public:
+    int _shapeId;
+    int _vertexId;
+    int _x;
+    int _y;
+    int _evtType;
+};
+
+#endif
