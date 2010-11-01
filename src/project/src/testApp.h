aef44b5f62a91e14fbd6cd4f8e4dd9a34f8657fa
jefa55@yahoo.com <jefa55@yahoo.com@49a7ce7b-0bdc-42b8-8dfc-eafa0b0813a6>
2010-11-01 00:09:10 +0000

    Proyecto usando ofxConsole y agregando Quads por consola

--- src/project/src/testApp.h
+++ src/project/src/testApp.h
@@ -1,6 +1,9 @@
 #ifndef _TEST_APP
 #define _TEST_APP
 
+//#include <vector>
+//#include <string>
+
 #include "ofMain.h"
 #include "Object3D.h"
 #include "Translation.h"
@@ -9,6 +12,8 @@
 #include "IEventListener.h"
 #include "Quad2D.h"
 #include "ofxMidiEvents.h"
+#include "ofxConsole.h";
+
 
 class testApp : public ofBaseApp, public IEventListener, public ofxMidiListener {
 
@@ -25,14 +30,18 @@ class testApp : public ofBaseApp, public IEventListener, public ofxMidiListener 
 		void mousePressed(int x, int y, int button);
 		void mouseReleased(int x, int y, int button);
 		void windowResized(int w, int h);
+        void quit(const std::vector<std::string> & args);
+
+        void setupLogging();
+        void setupConsole();
 
         void setPoint(int selectedIdx, int selectedVtx,
                       int x, int y, bool sendEvent=true);
-		int addQuad(int selIdx, bool sendEvent=true);
+		int addQuad(int selIdx, const char* label=NULL, bool sendEvent=true);
+		void addQuad(const std::vector<std::string> & args);
 
 		void event(EventArg *e);
 
-        void setupLogging();
         void saveQuads();
         void loadQuads();
 
@@ -40,6 +49,8 @@ class testApp : public ofBaseApp, public IEventListener, public ofxMidiListener 
 		Object3D *obj3D;
 		SynchManager *synchManager;
 		MidiManager	*midiManager;
+        ofxConsole * console;
+
 };
 
 #endif
