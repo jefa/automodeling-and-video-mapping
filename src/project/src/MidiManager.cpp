3cfc4c00ce7da6c9f442a462532b6ca09ed94740
jefa55@yahoo.com <jefa55@yahoo.com@49a7ce7b-0bdc-42b8-8dfc-eafa0b0813a6>
2010-11-03 02:12:16 +0000

    MidiEventArg que faltaba y midimanager refactoreado para que notifique a los listeners de los eventos, cuando amerita.

--- src/project/src/MidiManager.cpp
+++ src/project/src/MidiManager.cpp
@@ -1,10 +1,20 @@
 #include "MidiManager.h"
 #include "ofUtils.h"
+#include "MidiEventArg.h"
+
 
 MidiManager::MidiManager()
 {
+     midiIn.listPorts();
+     midiIn.openPort(0);
 
+     midiIn.addListener(this);
 
+	 // to register only to one controller pass the id as first argument
+     // midiIn.addListener(84,this);
+
+     // to debug
+     midiIn.setVerbose(true);
 }
 
 MidiManager::~MidiManager()
@@ -12,20 +22,9 @@ MidiManager::~MidiManager()
      ofLog(OF_LOG_VERBOSE, "MidiManager:: Destroying...");
 }
 
-void MidiManager::addListener(ofxMidiListener *listener, string evtKey)
+void MidiManager::addListener(IEventListener *listener, string evtKey)
 {
-
-
-     midiIn.listPorts();
-     midiIn.openPort(0);
-     midiIn.addListener(listener);
-
-	 // to register only to one controller pass the id as first argument
-     // midiIn.addListener(84,this);
-
-     // to debug
-     //midiIn.setVerbose(true);
-
+    listeners.insert( listenerPair(evtKey, listener) );
 }
 
 //midiIn events
@@ -34,6 +33,24 @@ void MidiManager::newMessage(int _port, int _id, int _value, double _timestamp){
 	id 			= _id;
 	port 		= _port;
 	timestamp 	= _timestamp;
-	cout<<"MidiManager::newMessage";
-}
+	cout<<"MidiManager::newMessage  Bulid Message ";
 
+
+	EventArg *evtArg;
+
+	// armar el mensaje a mandar  de ieventlistener
+	evtArg = new MidiEventArg();
+
+	((MidiEventArg*)evtArg)->_value = _value;
+	((MidiEventArg*)evtArg)->_id = _id;
+	((MidiEventArg*)evtArg)->_port = _port;
+	((MidiEventArg*)evtArg)->_timestamp = _timestamp;
+
+
+    map<string, IEventListener*>::iterator iter = listeners.begin();
+    while (iter != listeners.end() ) {
+        iter->second->event(evtArg);
+        iter++;
+    }
+
+}
