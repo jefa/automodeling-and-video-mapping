001f457be2fa0b7675cb68e30deb7fdf61eb18d9
eldano.v2 <eldano.v2@49a7ce7b-0bdc-42b8-8dfc-eafa0b0813a6>
2010-11-03 18:31:19 +0000

    Se agregó funcionalidad al TimeManager

--- trunk/src/project/src/TimeManager.h
+++ trunk/src/project/src/TimeManager.h
@@ -1,6 +1,8 @@
 #ifndef TIMEMANAGER_H
 #define TIMEMANAGER_H
 
+#include "IEventListener.h"
+
 #include <string>
 #include <map>
 using namespace std;
@@ -12,9 +14,11 @@ class TimeManager
         virtual ~TimeManager();
         static void Init();
         static void Update();
+        static void AddTimedEvent(float time, IEventListener *eventListener, string param);
+
     protected:
     private:
-        static map<float, string> events;
+        static map<float, pair<IEventListener*,string> > events;
 };
 
 #endif // TIMEMANAGER_H
