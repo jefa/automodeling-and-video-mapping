2c18d8f062162d1b1cafb050831555e1c0917a2e
adrianasoucoff <adrianasoucoff@49a7ce7b-0bdc-42b8-8dfc-eafa0b0813a6>
2010-11-06 21:00:36 +0000

    se saca index de addQuad

--- trunk/src/project/src/testApp.h
+++ trunk/src/project/src/testApp.h
@@ -38,7 +38,7 @@ class testApp : public ofBaseApp, public IEventListener, public ofxMidiListener 
 
         void setPoint(int selectedIdx, int selectedVtx,
                       int x, int y, bool sendEvent=true);
-		int addQuad(int selIdx, const char* label=NULL, bool sendEvent=true);
+		int addQuad(/*int selIdx, */const char* label=NULL, bool sendEvent=true);
 		void addQuad(const std::vector<std::string> & args);
 
 		void event(EventArg *e);
