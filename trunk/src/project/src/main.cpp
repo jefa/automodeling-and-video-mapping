966dfb0cfffc6b2ac4653796a9e0686bcd45d195
eldano.v2 <eldano.v2@49a7ce7b-0bdc-42b8-8dfc-eafa0b0813a6>
2010-11-09 21:35:44 +0000

    Se agregaron argumentos de resolucion y posicion de pantalla en el main.

--- trunk/src/project/src/main.cpp
+++ trunk/src/project/src/main.cpp
@@ -5,19 +5,51 @@
 //========================================================================
 int main( int argc, char** argv ) {
 
+    int minResWidth = 800;
+    int maxResWidth = 2048;
+    int minResHeight = 300;
+    int maxResHeight = 2048;
+    int minPosWinX = -2048;
+    int maxPosWinX = 2048;
+    int minPosWinY = 0;
+    int maxPosWinY = 1024;
+
     string nodeName = "noname";
-    if (argc > 1){
+    int resWidth, resHeight, posWinX, posWinY;
+    if (argc != 6){
+        ofLog(OF_LOG_ERROR, "Debe iniciar el programa con 6 argumentos: %s nodeName resWidth resHeight posWinX posWinY", argv[0]);
+        return -1;
+    }
+    else {
         nodeName = argv[1];
+        resWidth = atoi(argv[2]);
+        resHeight = atoi(argv[3]);
+        posWinX = atoi(argv[4]);
+        posWinY = atoi(argv[5]);
+
+        if(resWidth > maxResWidth || resWidth < minResWidth) {
+            ofLog(OF_LOG_ERROR, "el argumento resWidth debe estar entre %i y %i", minResWidth, maxResWidth);
+            return -1;
+        }
+        if(resHeight > maxResHeight || resHeight < minResHeight) {
+            ofLog(OF_LOG_ERROR, "el argumento resHeight debe estar entre %i y %i", minResHeight, maxResHeight);
+            return -1;
+        }
+        if(posWinX > maxPosWinX || posWinX < minPosWinX) {
+            ofLog(OF_LOG_ERROR, "el argumento posWinX debe estar entre %i y %i", minPosWinX, maxPosWinX);
+            return -1;
+        }
+        if(posWinY > maxPosWinY || posWinY < minPosWinY) {
+            ofLog(OF_LOG_ERROR, "el argumento posWinY debe estar entre %i y %i", minPosWinY, maxPosWinY);
+            return -1;
+        }
     }
 
     ofAppGlutWindow window;
-	ofSetupOpenGL(&window, 1024,768, OF_WINDOW);			// <-------- setup the GL context
+	ofSetupOpenGL(&window, resWidth, resHeight, OF_WINDOW);			// <-------- setup the GL context
 
-    #ifndef CONSOLE
-    ofSetWindowPosition(-1024,0);
-    #else
-    ofSetWindowPosition(200,20);
-    #endif
+    ofSetWindowPosition(posWinX, posWinY);
+
 
 	// this kicks off the running of my app
 	// can be OF_WINDOW or OF_FULLSCREEN
