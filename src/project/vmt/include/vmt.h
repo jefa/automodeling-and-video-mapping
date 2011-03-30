1535422305014f5f4758023398dba6de8661f1aa
eldano.v2@gmail.com <eldano.v2@gmail.com@49a7ce7b-0bdc-42b8-8dfc-eafa0b0813a6>
2011-03-30 14:01:35 +0000

    Primer commit de vmt

---
+++ src/project/vmt/include/vmt.h
@@ -0,0 +1,32 @@
+#ifndef _TEST_APP
+#define _TEST_APP
+
+#include "ofMain.h"
+
+#include "ofxCamera.h"
+#include "ofxLight.h"
+
+class vmt : public ofBaseApp {
+
+	public:
+        vmt();
+
+		void setup();
+		void update();
+		void draw();
+
+		void keyPressed(int key);
+		void keyReleased(int key);
+		void mouseMoved(int x, int y );
+		void mouseDragged(int x, int y, int button);
+		void mousePressed(int x, int y, int button);
+		void mouseReleased(int x, int y, int button);
+
+        float centerX, centerY, centerZ;
+        float camX, camY, camZ;
+
+    private:
+
+};
+
+#endif
---
+++ src/project/vmt/src/main.cpp
@@ -0,0 +1,25 @@
+#include "ofMain.h"
+#include "vmt.h"
+#include "ofAppGlutWindow.h"
+
+//========================================================================
+int main( int argc, char** argv ) {
+
+    int resWidth, resHeight, posWinX, posWinY;
+
+    resWidth = 800;
+    resHeight = 600;
+    posWinX = 50;
+    posWinY = 50;
+
+    ofAppGlutWindow window;
+	ofSetupOpenGL(&window, resWidth, resHeight, OF_WINDOW);			// <-------- setup the GL context
+
+    ofSetWindowPosition(posWinX, posWinY);
+
+	// this kicks off the running of my app
+	// can be OF_WINDOW or OF_FULLSCREEN
+	// pass in width and height too:
+	ofRunApp( new vmt());
+
+}
