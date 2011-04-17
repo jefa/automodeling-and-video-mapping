f271c8ff4a97ba7e425a0cf5eeef1efcdc6f370f
jefa55@yahoo.com <jefa55@yahoo.com@49a7ce7b-0bdc-42b8-8dfc-eafa0b0813a6>
2011-04-17 22:14:16 +0000

    vmt_node project. primera version no funcional.

---
+++ src/project/vmt_node/include/vmtnode.h
@@ -0,0 +1,46 @@
+#ifndef _VMTNODE
+#define _VMTNODE
+
+#include "ofMain.h"
+#include "ofxOsc.h"
+
+#include "Scene.h"
+
+// listen on port 12345
+#define PORT 12345
+#define NUM_MSG_STRINGS 20
+
+
+//--------------------------------------------------------
+class VmtNode : public ofBaseApp{
+
+	public:
+
+		void setup();
+		void update();
+		void draw();
+
+		void keyPressed  (int key);
+		void mouseMoved(int x, int y );
+		void mouseDragged(int x, int y, int button);
+		void mousePressed(int x, int y, int button);
+		void mouseReleased(int x, int y, int button);
+		void windowResized(int w, int h);
+
+		ofTrueTypeFont		font;
+
+
+private:
+		ofxOscReceiver	receiver;
+
+		int				current_msg_string;
+		string		msg_strings[NUM_MSG_STRINGS];
+		float			timers[NUM_MSG_STRINGS];
+
+		int				mouseX, mouseY;
+		string			mouseButtonState;
+
+		Scene *scene;
+};
+
+#endif
