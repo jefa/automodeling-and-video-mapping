9269ab3509a4197954fb07b2124ab20510a04166
jefa55@yahoo.com <jefa55@yahoo.com@49a7ce7b-0bdc-42b8-8dfc-eafa0b0813a6>
2011-05-02 05:07:17 +0000

    ventana de edicion de efectos

---
+++ trunk/src/project/vmt/src/effecteditdialog.cpp
@@ -0,0 +1,77 @@
+#include <QtGui>
+
+#include "effecteditdialog.h"
+#include "effectpages.h"
+
+EffectEditDialog::EffectEditDialog()
+{
+    contentsWidget = new QListWidget;
+    contentsWidget->setViewMode(QListView::IconMode);
+    contentsWidget->setIconSize(QSize(96, 84));
+    contentsWidget->setMovement(QListView::Static);
+    contentsWidget->setMaximumWidth(128);
+    contentsWidget->setSpacing(12);
+
+    pagesWidget = new QStackedWidget;
+    pagesWidget->addWidget(new PositionEffectPage);
+    pagesWidget->addWidget(new FadeEffectPage);
+    pagesWidget->addWidget(new TextureEffectPage);
+
+    QPushButton *closeButton = new QPushButton(tr("Close"));
+
+    createIcons();
+    contentsWidget->setCurrentRow(0);
+
+    connect(closeButton, SIGNAL(clicked()), this, SLOT(close()));
+
+    QHBoxLayout *horizontalLayout = new QHBoxLayout;
+    horizontalLayout->addWidget(contentsWidget);
+    horizontalLayout->addWidget(pagesWidget, 1);
+
+    QHBoxLayout *buttonsLayout = new QHBoxLayout;
+    buttonsLayout->addStretch(1);
+    buttonsLayout->addWidget(closeButton);
+
+    QVBoxLayout *mainLayout = new QVBoxLayout;
+    mainLayout->addLayout(horizontalLayout);
+    mainLayout->addStretch(1);
+    mainLayout->addSpacing(12);
+    mainLayout->addLayout(buttonsLayout);
+    setLayout(mainLayout);
+
+    setWindowTitle(tr("Config Dialog"));
+}
+
+void EffectEditDialog::createIcons()
+{
+    QListWidgetItem *configButton = new QListWidgetItem(contentsWidget);
+    //configButton->setIcon(QIcon(":/images/config.png"));
+    configButton->setIcon(QIcon("/images/config.png"));
+    configButton->setText(tr("Position"));
+    configButton->setTextAlignment(Qt::AlignHCenter);
+    configButton->setFlags(Qt::ItemIsSelectable | Qt::ItemIsEnabled);
+
+    QListWidgetItem *updateButton = new QListWidgetItem(contentsWidget);
+    updateButton->setIcon(QIcon(":/images/update.png"));
+    updateButton->setText(tr("Fade"));
+    updateButton->setTextAlignment(Qt::AlignHCenter);
+    updateButton->setFlags(Qt::ItemIsSelectable);
+
+    QListWidgetItem *queryButton = new QListWidgetItem(contentsWidget);
+    queryButton->setIcon(QIcon(":/images/query.png"));
+    queryButton->setText(tr("Texture"));
+    queryButton->setTextAlignment(Qt::AlignHCenter);
+    queryButton->setFlags(Qt::ItemIsSelectable | Qt::ItemIsEnabled);
+
+    connect(contentsWidget,
+            SIGNAL(currentItemChanged(QListWidgetItem*,QListWidgetItem*)),
+            this, SLOT(changePage(QListWidgetItem*,QListWidgetItem*)));
+}
+
+void EffectEditDialog::changePage(QListWidgetItem *current, QListWidgetItem *previous)
+{
+    if (!current)
+        current = previous;
+
+    pagesWidget->setCurrentIndex(contentsWidget->row(current));
+}
