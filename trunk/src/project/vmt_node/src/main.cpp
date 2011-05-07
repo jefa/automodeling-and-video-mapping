09e8458b29ff6bc672f646f61a6a3185df832667
eldano.v2@gmail.com <eldano.v2@gmail.com@49a7ce7b-0bdc-42b8-8dfc-eafa0b0813a6>
2011-05-07 17:56:56 +0000

    se agregaron argumentos al vmtnode

--- trunk/src/project/vmt_node/src/main.cpp
+++ trunk/src/project/vmt_node/src/main.cpp
@@ -3,10 +3,56 @@
 #include "ofAppGlutWindow.h"
 
 //========================================================================
-int main( ){
+const int minResWidth = 800;
+const int maxResWidth = 2048;
+const int minResHeight = 300;
+const int maxResHeight = 2048;
+const int minPosWinX = -2048;
+const int maxPosWinX = 2048;
+const int minPosWinY = 0;
+const int maxPosWinY = 1024;
+
+int main( int argc, char** argv ) {
+
+    int resWidth, resHeight, posWinX, posWinY;
+    if (argc != 5 && argc != 1){
+        ofLog(OF_LOG_ERROR, "Debe iniciar el programa con 5 argumentos: %s resWidth resHeight posWinX posWinY", argv[0]);
+        return -1;
+    }
+    else if(argc == 1) {
+        ofLog(OF_LOG_WARNING, "Iniciando el programa con la configuración de pantalla por defecto");
+        resWidth = 640;
+        resHeight = 480;
+        posWinX = 50;
+        posWinY = 50;
+    }
+    else {
+        resWidth = atoi(argv[1]);
+        resHeight = atoi(argv[2]);
+        posWinX = atoi(argv[3]);
+        posWinY = atoi(argv[4]);
+
+        if(resWidth > maxResWidth || resWidth < minResWidth) {
+            ofLog(OF_LOG_ERROR, "el argumento resWidth debe estar entre %i y %i", minResWidth, maxResWidth);
+            return -1;
+        }
+        if(resHeight > maxResHeight || resHeight < minResHeight) {
+            ofLog(OF_LOG_ERROR, "el argumento resHeight debe estar entre %i y %i", minResHeight, maxResHeight);
+            return -1;
+        }
+        if(posWinX > maxPosWinX || posWinX < minPosWinX) {
+            ofLog(OF_LOG_ERROR, "el argumento posWinX debe estar entre %i y %i", minPosWinX, maxPosWinX);
+            return -1;
+        }
+        if(posWinY > maxPosWinY || posWinY < minPosWinY) {
+            ofLog(OF_LOG_ERROR, "el argumento posWinY debe estar entre %i y %i", minPosWinY, maxPosWinY);
+            return -1;
+        }
+    }
 
     ofAppGlutWindow window;
-	ofSetupOpenGL(&window, 640,480, OF_WINDOW);			// <-------- setup the GL context
+	ofSetupOpenGL(&window, resWidth, resHeight, OF_WINDOW);			// <-------- setup the GL context
+    ofSetWindowPosition(posWinX, posWinY);
 
     window.setWindowTitle("Vmt Node");
 
@@ -16,3 +62,7 @@ int main( ){
 	ofRunApp( new VmtNode());
 
 }
+
+
+    string nodeName = "noname";
+
