e076e828684c1a5875910cfad51f7c8a0c7fb58e
jefa55@yahoo.com <jefa55@yahoo.com@49a7ce7b-0bdc-42b8-8dfc-eafa0b0813a6>
2012-01-18 00:47:50 +0000

    modificacion al post launch del compilador.

--- src/project/vmt/src/vmt.cpp
+++ src/project/vmt/src/vmt.cpp
@@ -23,7 +23,7 @@ void Vmt::setup(){
 	glEnable(GL_DEPTH_TEST); //lights look weird if depth test is not enabled
 
     vmtModel = new VmtModel();
-    vmtModel->loadShow("showCubos.XML");
+    vmtModel->loadShow("./showCubos.XML");
 
     //vmtModel->saveShow("showYYY.XML");
 
